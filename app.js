637ad70026bc570cdc986d9bd84f8ba23c9c2429
yun <n856839@gmail.com>
2021-02-01 16:05:20 +0800

    Add confirm user password, unique email, express-session & connect-flash

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models')
+
 const app = express()
 const port = 3000
 
 app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true })) // body-parser
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
