0d89f581ab1830b96728b193262c9f0deaacd727
yun <n856839@gmail.com>
2021-02-01 19:36:33 +0800

    Add authentication before render page & isAdmin to users table

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,25 @@ const passport = require('passport')
 
 
 module.exports = (app, passport) => {
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
 
-  app.get('/', (req, res) => res.redirect('/restaurants'))
-  app.get('/restaurants', restController.getRestaurants)
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', authenticated, restController.getRestaurants)
 
-  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
