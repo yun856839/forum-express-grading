36921a626f8862e9a9bd359a26e9882d781cb67c
yun <n856839@gmail.com>
2021-02-01 20:08:27 +0800

    Add admin restaurant index page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,13 @@
-let adminController = {
+const db = require('../models')
+const Restaurant = db.Restaurant
+
+
+const adminController = {
   getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+    // {raw: true} 轉換成 JS 原生物件。
+    return Restaurant.findAll({ raw: true }).then(restaurants => {
+      return res.render('admin/restaurants', { restaurants })
+    })
   }
 }
 
