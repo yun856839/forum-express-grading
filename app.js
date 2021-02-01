08b1fffa5f42eebe64108c1c3762e22c1cf7b71f
yun <n856839@gmail.com>
2021-02-01 20:57:15 +0800

    Add admin restaurant put feature use method-override

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const db = require('./models')
 const passport = require('./config/passport')
+const methodOverride = require('method-override')
 
 const app = express()
 const port = 3000
@@ -19,6 +20,7 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
+app.use(methodOverride('_method'))
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
