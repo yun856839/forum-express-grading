25763649c0ad888aae6935ca08dd0331486a3164
yun <n856839@gmail.com>
2021-02-02 13:31:09 +0800

    Add procfile

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const passport = require('./config/passport')
 const methodOverride = require('method-override')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
