3caea84c09e1c1e0f5fa91a4a77d11ed9e2dc2a1
yun <n856839@gmail.com>
2021-02-09 12:06:51 +0800

    Add pagination on restaurants index

--- controllers/restController.js
+++ controllers/restController.js
@@ -2,17 +2,30 @@ const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
 
+const pageLimit = 10 //一頁顯示 10 筆
 
 const restController = {
   getRestaurants: (req, res) => {
     const whereQuery = {}
     let categoryId = ''
+    let offset = 0
+
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
+
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
       whereQuery.CategoryId = categoryId
     }
-    Restaurant.findAll({ include: Category, where: whereQuery }).then(restaurants => {
-      const data = restaurants.map(r => ({
+    Restaurant.findAndCountAll({ include: Category, where: whereQuery, offset, limit: pageLimit }).then(result => {
+      const page = Number(req.query.page) || 1 // 頁數沒有零
+      const pages = Math.ceil(result.count / pageLimit) //最大頁數
+      const totalPage = Array.from({ length: pages }).map((item, index) => index + 1) // 總共有幾頁 1.2.3.4.5.6
+      const prev = page - 1 < 1 ? 1 : page - 1
+      const next = page + 1 < pages ? pages : page + 1
+
+      const data = result.rows.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
         categoryName: r.Category.name
@@ -24,7 +37,11 @@ const restController = {
         return res.render('restaurants', {
           restaurants: data,
           categories,
-          categoryId
+          categoryId,
+          page,
+          totalPage,
+          prev,
+          next
         })
       })
     })
