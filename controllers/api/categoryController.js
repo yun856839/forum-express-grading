a8b672e8a0bfbaa7391d23d85f170d7b7d2da8c7
yun <n856839@gmail.com>
2021-02-17 22:34:38 +0800

    DELETE /api/admin/categories/:id

--- controllers/api/categoryController.js
+++ controllers/api/categoryController.js
@@ -19,5 +19,10 @@ const categoryController = {
       return res.json(data)
     })    
   },
+  deleteCategory: (req, res) => {
+    categoryService.deleteCategory(req, res, (data) => {
+       return res.json(data)
+    })    
+  }
 }
 module.exports = categoryController
--- routes/apis.js
+++ routes/apis.js
@@ -14,5 +14,6 @@ router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 router.get('/admin/categories', categoryController.getCategories)
 router.post('/admin/categories', categoryController.postCategory)
 router.put('/admin/categories/:id', categoryController.putCategory)
+router.delete('/admin/categories/:id', categoryController.deleteCategory)
 
 module.exports = router
