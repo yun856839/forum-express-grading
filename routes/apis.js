b9542324d67229b7c0d3fd92848b77e3449301dc
yun <n856839@gmail.com>
2021-02-18 12:20:59 +0800

    Add api/signin page, install jsonwebtoken & passport-jwt

--- routes/apis.js
+++ routes/apis.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 const adminController = require('../controllers/api/adminController.js')
-const categoryController = require('../controllers/api/categoryController')
+const categoryController = require('../controllers/api/categoryController.js')
+const userController = require('../controllers/api/userController.js')
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get('/admin/restaurants/:id', adminController.getRestaurant)
@@ -15,5 +16,7 @@ router.get('/admin/categories', categoryController.getCategories)
 router.post('/admin/categories', categoryController.postCategory)
 router.put('/admin/categories/:id', categoryController.putCategory)
 router.delete('/admin/categories/:id', categoryController.deleteCategory)
+
+router.post('/signin', userController.signIn)
 
 module.exports = router
